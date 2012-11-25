31db734c95bf1044506f4a605229702a6f1e2d70
Oleg Haidul <oleghaidul@gmail.com>
2012-11-25 19:22:55 +0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+# Plugin's routes
+# See: http://guides.rubyonrails.org/routing.html
+
+resources :people do
+    collection do
+      get :bulk_edit, :context_menu, :edit_mails, :preview_email, :avatar
+      post :bulk_edit, :bulk_update, :send_mails
+      delete :bulk_destroy
+    end
+end
+
+resources :people_settings do
+  collection do
+    get :autocomplete_for_user
+  end
+end  
+# match "people_settings/:action" => "people_settings"
+
