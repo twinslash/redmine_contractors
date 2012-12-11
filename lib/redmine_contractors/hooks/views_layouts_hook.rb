e8069fe7d06ff882475d51d6f6d31cf5d5d1b1bf
Oleg Haidul <oleghaidul@gmail.com>
2012-12-11 10:47:54 +0300

    Change en locale

--- lib/redmine_contractors/hooks/views_layouts_hook.rb
+++ lib/redmine_contractors/hooks/views_layouts_hook.rb
@@ -2,7 +2,7 @@ module RedminePeople
   module Hooks
     class ViewsLayoutsHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        return stylesheet_link_tag(:people, :plugin => 'redmine_contractors')
+        return stylesheet_link_tag(:persons, :plugin => 'redmine_contractors')
       end
     end
   end
