31694ae24805d665b8f3f16e8ca3cdf14c49cf75
Oleg Haidul <oleghaidul@gmail.com>
2013-03-06 09:16:59 +0300

    Update person-token-input.js
    Try to fix auth error

--- assets/javascripts/person-token-input.js
+++ assets/javascripts/person-token-input.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#person_skill_list').tokenInput('/people/skills.json', {
-    crossDomain: false,
+    crossDomain: true,
     theme: 'facebook',
     preventDuplicates: true,
     prePopulate: $("#person_skill_list").data("pre"),
@@ -9,7 +9,7 @@ $(document).ready(function() {
   });
 
   $('#person_foreign_language_list').tokenInput('/people/foreign_languages.json', {
-    crossDomain: false,
+    crossDomain: true,
     theme: 'facebook',
     preventDuplicates: true,
     prePopulate: $("#person_foreign_language_list").data("pre"),
@@ -18,7 +18,7 @@ $(document).ready(function() {
   });
 
   $('#person_hobby_list').tokenInput('/people/hobbies.json', {
-    crossDomain: false,
+    crossDomain: true,
     theme: 'facebook',
     preventDuplicates: true,
     prePopulate: $("#person_hobby_list").data("pre"),
